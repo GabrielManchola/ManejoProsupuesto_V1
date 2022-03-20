5000d94a478ab261e4c27c525ca6a27b98567b6e
GabrielManchola <50031687+GabrielManchola@users.noreply.github.com>
2022-03-20 10:56:12 -0500

    detalle de cuentas y movimientos

--- ManejoPresupuesto/Controllers/CuentasController.cs
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -198,6 +198,11 @@ namespace ManejoPresupuesto.Controllers
             modelo.FechaInicio = fechaInicio;
             modelo.FechaFin = fechaFin;
 
+            ViewBag.mesAnterior = fechaInicio.AddMonths(-1).Month;
+            ViewBag.añoAnterior = fechaInicio.AddMonths(-1).Year;
+            ViewBag.mesPosterior = fechaFin.AddMonths(1).Month;
+            ViewBag.añoPosterior = fechaFin.AddMonths(1).Year;
+
             return View(modelo);
 
         }
