938792a84d914e15245fdc71490b8c1f5290b752
GabrielManchola <50031687+GabrielManchola@users.noreply.github.com>
2022-04-15 18:07:54 -0500

    comienzo con el  controlador y formulario de usuarios

---
+++ ManejoPresupuesto/Models/RegistroViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe ser un correo electronico valido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
